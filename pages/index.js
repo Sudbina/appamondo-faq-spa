c7ad6e0e3c1c6ec76b817089a6057979ada1f489
Jack Gibson <enozorez@gmail.com>
2021-04-14 23:18:51 +0100

    add article data for flexibility and cleaner code

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,15 @@ import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { LoremIpsum } from 'react-lorem-ipsum';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { articleData } from '../article-meta.js';
 
 export default function Home() {
   const [content, setContent] = useState('home');
 
-  const updateContent = (content) => {
-    console.log(content);
-    setContent(content);
+  const updateContent = (key, articleItem) => {
+    console.log('Updating content to ', articleItem);
+    setContent(key);
   };
 
   const back = () => {
@@ -23,71 +24,45 @@ export default function Home() {
         <title>Appamondo - FAQ</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
+      <Header />
       <main>
-        <Header />
         <section className='hero'>
           <h1 className='hero-title'>Got questions? We have answers.</h1>
           <h4 className='hero-description'>
-            Can't find the answer you're looking for?{' '}
-            <a href='#'>Get in touch.</a>
+            Can't find the answer you're looking for?
+            <a href='#'> Get in touch.</a>
           </h4>
         </section>
         <section className='content-wrapper'>
           {content === 'home' ? (
             <div className='container'>
               <Row className='d-flex justify-content-between mt-5'>
-                <section
-                  className='content-card left'
-                  onClick={() => updateContent('left')}
-                >
-                  <h2 className='content-title'>
-                    <img
-                      className='icon'
-                      src='/images/icons/white/BESPOKE.png'
-                    />
-                    Custom Solutions
-                  </h2>
-                  <p>
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Eget
-                    dictum quisque hendrerit torquent semper posuere arcu
-                    nostra.
-                  </p>
-                  <button>View Article</button>
-                </section>
-                <section
-                  className='content-card middle'
-                  onClick={() => updateContent('middle')}
-                >
-                  <h2>
-                    <img className='icon' src='/images/icons/white/SCOPE.png' />
-                    Zendesk Products
-                  </h2>
-                  <p>
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Eget
-                    dictum quisque hendrerit torquent semper posuere arcu
-                    nostra.
-                  </p>
-                  <button>View Article</button>
-                </section>
-                <section className='content-card right'>
-                  <h2>
-                    <img
-                      className='icon'
-                      src='/images/icons/white/SUPPORT.png'
-                    />
-                    Client Support
-                  </h2>
-                  <p>
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Eget
-                    dictum quisque hendrerit torquent semper posuere arcu
-                    nostra.
-                  </p>
-                  <button disabled>View Article</button>
-                </section>
+                {articleData.map((articleItem, key) => (
+                  <div
+                    className={`content-card-${articleItem.key}`}
+                    onClick={
+                      !articleItem.disabled
+                        ? () => updateContent(key, articleItem)
+                        : null
+                    }
+                  >
+                    <h2 className='content-title'>
+                      <img className='icon' src={articleItem.icon} />
+                      {articleItem.title}
+                    </h2>
+                    <p>
+                      Lorem ipsum odor amet, consectetuer adipiscing elit. Eget
+                      dictum quisque hendrerit torquent semper posuere arcu
+                      nostra.
+                    </p>
+                    <button disabled={articleItem.disabled}>
+                      {articleItem.disabled ? 'Coming Soon...' : 'View Article'}
+                    </button>
+                  </div>
+                ))}
               </Row>
             </div>
-          ) : content === 'left' ? (
+          ) : content === 0 ? (
             <div className='container' style={{ paddingTop: 20 }}>
               <span onClick={() => back()}>Back</span>
               <h1 className='article-title'>Custom Solutions</h1>
@@ -97,7 +72,7 @@ export default function Home() {
               <hr />
               <LoremIpsum p={5} />
             </div>
-          ) : content === 'middle' ? (
+          ) : content === 1 ? (
             <div className='container' style={{ paddingTop: 20 }}>
               <span onClick={() => back()}>Back</span>
               <h1 className='article-title'>Zendesk Products</h1>
