f284b3d3377cbd637ab60acbbe210463be76e523
Jack Gibson <enozorez@gmail.com>
2021-04-15 11:18:19 +0100

    optimise stock images (even further)

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { isMobile } from 'react-device-detect';
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import menuAlt from '@iconify/icons-dashicons/menu-alt';
 import adminHome from '@iconify/icons-dashicons/admin-home';
 import editorHelp from '@iconify/icons-dashicons/editor-help';
 
-export default function Header({ title }) {
+export default function Header() {
   const [menuVisible, setVisibility] = useState(false);
   return (
     <>
