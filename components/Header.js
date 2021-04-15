0aa74d19616fa239684c5446cacaf88c4a14b6fb
Jack Gibson <enozorez@gmail.com>
2021-04-15 11:05:31 +0100

    finish touching up mobile menu
    this was literally for my own sanity.

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { isMobile } from 'react-device-detect';
 import { Icon, InlineIcon } from '@iconify/react';
 import menuAlt from '@iconify/icons-dashicons/menu-alt';
+import adminHome from '@iconify/icons-dashicons/admin-home';
+import editorHelp from '@iconify/icons-dashicons/editor-help';
 
 export default function Header({ title }) {
   const [menuVisible, setVisibility] = useState(false);
@@ -13,10 +15,18 @@ export default function Header({ title }) {
           onClick={() => setVisibility(false)}
         >
           <div className='header-mobile-menu'>
+            <img
+              className='menu-logo'
+              src='../images/logos/appamondo-logo-only.png'
+            />
             <ul className='menu-inner'>
-              <li>Home</li>
-              <li>FAQ</li>
-              <li>Log In</li>
+              <li>
+                <Icon icon={adminHome} /> Home
+              </li>
+              <li>
+                <Icon icon={editorHelp} /> FAQ
+              </li>
+              <li className='menu-button'>Log In</li>
             </ul>
           </div>
         </div>
